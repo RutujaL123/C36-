2046a302eb0f65965bd3a0aa8479359d34223bb9
Rutuja <96522053+RutujaL123@users.noreply.github.com>
2022-09-12 21:05:11 +0530

    Add files via upload

---
+++ Contestant.js
@@ -0,0 +1,15 @@
+class Contestant {
+  constructor(){
+  this.button.mousePressed(()=>{ 
+  this.message.html("Thank You, Your Answer Has Been Submitted"); 
+  this.message.position(350, 350);
+  this.message.html("Thank You, Your Answer Has Been Submitted"); 
+  this.message.position();
+  this.message("Thank You, Your Answer Has Been Submitted");
+  this.message(350, 350); 
+  this.message.html("Thank You, Your Answer Has Been Submitted");  
+  this.message.position(350); });
+  }
+
+  
+}
